52f3bf34a32bac5273c7c9d960e51d87a147d59c
MATEY15 <xzzmanzzx@gmail.com>
2018-10-24 18:31:48 +0300

    Version 1.0

---
+++ app/js/common.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+	$('').click(function(e){
+		e.preventDefault();
+		$('').toggleClass('is-active');
+	});
+
+	/* Popup Window */
+
+	// $(".popup").magnificPopup({
+	// 	removalDelay: 150,
+	// 	mainClass: 'mfp-fade'
+	// });
+
+/*
+	$('.slide-section').owlCarousel({
+		loop: false,
+		singleItem:true,
+		nav:true,
+		margin: 1,
+		autoplayTimeout: 1500,
+		navText : ['',''],
+		autoplayHoverPause:true,
+		smartSpeed: 300,
+		items:1,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			640:{
+				items:1
+			},
+			900:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	});
+*/
+});
